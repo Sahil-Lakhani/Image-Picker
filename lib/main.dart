769954c47da721d38805fca82288eb9bde6a0987
Sahil-Lakhani <sahillakhani266@gmail.com>
2024-03-27 14:44:59 +0530

    completed the functinality for selecting an image

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:custom_image_app/image_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,15 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Image Selection Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
+      debugShowCheckedModeBanner: false,
       home: const ProfileScreen(),
     );
   }
@@ -26,16 +37,80 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
-      appBar: AppBar(title: const Text('Image Picker & cropper')),
+      appBar: AppBar(
+        title: const Text('Image Picker & cropper'),
+      ),
       body: ListView(
         padding: const EdgeInsets.symmetric(
           vertical: 80,
           horizontal: 20,
         ),
-        children: const [],
+        children: const [ProfileImage(initials: 'ZS')],
       ),
     );
+  }
+}
+
+final imageHelper = ImageHelper();
+
+class ProfileImage extends StatefulWidget {
+  const ProfileImage({super.key, required this.initials});
+  final String initials;
+
+  @override
+  State<ProfileImage> createState() => _ProfileImageState();
+}
+
+class _ProfileImageState extends State<ProfileImage> {
+  File? _image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      Center(
+        child: _image != null
+            ? Image.file(
+                _image!,
+                width: 228,
+                height: 228,
+                fit: BoxFit.cover,
+              )
+            : Container(
+                width: 128,
+                height: 128,
+                color: Colors.grey[300],
+                child: Center(
+                  child: Text(
+                    widget.initials,
+                    style: const TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+      ),
+      const SizedBox(height: 20),
+      TextButton(
+          onPressed: () async {
+            final files = await imageHelper.pickImage();
+            if (files.isNotEmpty) {
+              final file = files.first;
+              if (file != null) {
+                final croppedFile = await imageHelper.cropImage(
+                  file: file,
+                  cropStyle: CropStyle.rectangle,
+                );
+                if (croppedFile != null) {
+                  setState(() {
+                    _image = File(croppedFile.path);
+                  });
+                }
+              }
+            }
+          },
+          child: const Text('Select Image '))
+    ]);
   }
 }
